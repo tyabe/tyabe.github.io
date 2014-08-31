a8916b1d4923bebc44f4f4844ff9021247210701
Takeshi Yabe <tyabe@nilidea.com>
2014-09-01 00:44:18 +0900

    Redesign

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem "middleman", "~>3.3.5"
 gem "middleman-livereload", "~> 3.1.0"
 gem "middleman-deploy"
 gem "middleman-google-analytics"
+gem "middleman-favicon-maker", "~> 3.7"
 
 gem "slim"
 gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
--- config.rb
+++ config.rb
@@ -63,6 +63,21 @@ set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
+  activate :favicon_maker do |f|
+    f.template_dir  = File.join(root, 'source', 'images')
+    f.output_dir    = File.join(root, 'build')
+    f.icons = {
+      "logo.png" => [
+        { icon: "apple-touch-icon-152x152-precomposed.png" },
+        { icon: "apple-touch-icon-144x144-precomposed.png" },
+        { icon: "apple-touch-icon-114x114-precomposed.png" },
+        { icon: "apple-touch-icon-72x72-precomposed.png" },
+        { icon: "favicon.png", size: "16x16" },
+        { icon: "favicon.ico", size: "64x64,32x32,24x24,16x16" },
+      ]
+    }
+  end
+
   # For example, change the Compass output style for deployment
   activate :minify_css
 
@@ -87,4 +102,12 @@ end
 activate :google_analytics do |ga|
   ga.tracking_id = 'UA-54111952-1'
   ga.development = true
+end
+
+require 'digest/md5'
+helpers do
+  def gravatar_for(email)
+    hash = Digest::MD5.hexdigest(email.chomp.downcase)
+    "http://www.gravatar.com/avatar/#{hash}"
+  end
 end
